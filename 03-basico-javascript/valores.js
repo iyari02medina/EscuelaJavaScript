502abd91eed1038f80947fb802cd044a05d8f118
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-03-22 12:52:07 -0600

    tipos de valores en javascript

---
+++ 03-basico-javascript/valores.js
@@ -0,0 +1,10 @@
+40 // numero 
+"Hola" // string
+true // booleano 
+false //booleano 
+
+null // valor place holder
+undefined // valor placeholder
+
+[1, 2, 3] // array
+{nombre: "iyari"} // objeto 
