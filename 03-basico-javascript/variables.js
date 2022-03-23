b9445cdaac738659f8ac5db843a0559e73d9b278
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-03-23 06:28:10 -0600

    tipos de valores javascript

---
+++ 03-basico-javascript/variables.js
@@ -0,0 +1,14 @@
+var nombre = "Iyari";
+
+// declarar 
+var edad;
+// inicializar
+edad = 30;
+
+// varible array con declaracion e iniciacion 
+var elementos = ["Computadora", "Celular"];
+//varible de tipo objeto 
+var persona = {
+    nombre: "Iyari",
+    edad: 25
+}
