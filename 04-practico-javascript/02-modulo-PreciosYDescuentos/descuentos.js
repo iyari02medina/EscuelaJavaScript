6001a1992a78fd0d247b872ce4a221ee013a265a
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-03-28 15:16:12 -0600

    calculadora de descuentos terminada

--- 04-practico-javascript/02-modulo-PreciosYDescuentos/descuentos.js
+++ 04-practico-javascript/02-modulo-PreciosYDescuentos/descuentos.js
@@ -4,6 +4,25 @@ function calcularPrecioConDescuento(precio, descuento) {
 
   return precioConDescuento;
 }
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  
+}
+
+// / / / / / / / / / / / / Cupones de descuento / / / / / / / / / / / /
+
 function onClickButtonPriceDiscountCoupon (){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
@@ -37,42 +56,6 @@ function onClickButtonPriceDiscountCoupon (){
       resultP.innerText = "El precio con descuento son: $ " + precioDescuento;
     }
 }
-
-function onClickButtonPriceDiscount() {
-  const inputPrice = document.getElementById("InputPrice");
-  const priceValue = inputPrice.value;
-
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
-
-  /* const inputCoupon = document.getElementById("InputCupon");
-  const couponValue = inputCoupon.value; */
-
-  const precioConDescuento = calcularPrecioConDescuento(
-    priceValue,
-    discountValue
-  );
-  const resultP = document.getElementById("ResultP");
-  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
-
-  /* const isCouponValueValid = function (coupon) {
-    return coupon.name === couponValue;
-  };
-  const userCoupon = coupons.find(isCouponValueValid);
-
-  if (!userCoupon) {
-    alert("El cupon " + userCoupon + " no es valido");
-  } else {
-    const descuento = userCoupon.discount;
-    const precioDescuento = calcularPrecioConDescuento(precioConDescuento, descuento);
-
-    const resultP = document.getElementById("ResultCoupon");
-    resultP.innerText = "El precio con descuento son: $ " + precioDescuento;
-  } */
-}
-
-// / / / / / / / / / / / / Cupones de descuento / / / / / / / / / / / /
-
 const coupons = [
   {
     name: "fito-es-batman",
