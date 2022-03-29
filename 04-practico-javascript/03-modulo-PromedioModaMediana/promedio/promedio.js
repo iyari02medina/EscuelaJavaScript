23d31220ae6e9b843f22fe4e6f96f8cf0b5523c7
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-03-29 12:52:09 -0600

    se creo la logica para el promedio ponderado

--- 04-practico-javascript/03-modulo-PromedioModaMediana/promedio/promedio.js
+++ 04-practico-javascript/03-modulo-PromedioModaMediana/promedio/promedio.js
@@ -1,3 +1,5 @@
+// / / / / / / / / / / / / / / / Promedio normal / / / / / / / / / / / / / / / /
+
 function calcularMediaAritmetica(lista) {
   /* let sumaLista = 0;
   for (let i = 0; i < lista.length; i++) {
@@ -10,3 +12,42 @@ function calcularMediaAritmetica(lista) {
   const promedioLista = sumaLista / lista.length;
   return promedioLista;
 }
+
+// / / / / / / / / / / / / / / / / Promedio Ponderado / / / / / / / // / / / / / / /
+const notes = [
+  {
+    course: "Educación Física",
+    note: 10,
+    credit: 2,
+  },
+  {
+    course: "Programación",
+    note: 8,
+    credit: 5,
+  },
+  {
+    course: "Finanzas personales",
+    note: 7,
+    credit: 5,
+  },
+];
+
+
+const notesWithCredit = notes.map(function (noteObject) {
+  return noteObject.note * noteObject.credit;
+});
+
+const sumOfNotesWithCredit = notesWithCredit.reduce(function (sum = 0, newVal) {
+  return sum + newVal;
+});
+
+const credits = notes.map(function (noteObject) {
+  return noteObject.credit;
+});
+
+const sumOfCredits = credits.reduce(function (sum = 0, newVal) {
+  return sum + newVal;
+});
+
+const promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
+
