0299a64b6a7177003c39e4ce85d8d5b7e3523872
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-03-30 07:47:53 -0600

    logica de analisis salarial

---
+++ 04-practico-javascript/04-modulo-AnalisisSalarial/salarios.js
@@ -0,0 +1,70 @@
+const colombia = [];
+
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+  name: "Fito",
+  salary: 1500,
+});
+colombia.push({
+  name: "Japlin",
+  salary: 2000,
+});
+colombia.push({
+  name: "Charly",
+  salary: 1800,
+});
+colombia.push({
+  name: "Trufa",
+  salary: 2100,
+});
+colombia.push({
+  name: "Mocho",
+  salary: 800,
+});
+colombia.push({
+  name: "Yhuan",
+  salary: 600,
+});
+colombia.push({
+  name: "Molly",
+  salary: 1100,
+});
+colombia.push({
+  name: "Bernarda",
+  salary: 750,
+});
+colombia.push({
+  name: "Nala",
+  salary: 1400,
+});
+colombia.push({
+  name: "Menta",
+  salary: 980,
+});
+colombia.push({
+  name: "Dante",
+  salary: 1000,
+});
+colombia.push({
+  name: "Alexia",
+  salary: 1200,
+});
+colombia.push({
+  name: "Erika",
+  salary: 750,
+});
+colombia.push({
+  name: "Itzel",
+  salary: 1000,
+});
+colombia.push({
+  name: "Ana",
+  salary: 2200,
+});
+colombia.push({
+  name: "Iyari",
+  salary: 3000,
+});
