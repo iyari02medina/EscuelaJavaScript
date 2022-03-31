4fda336e58d3c4e8ded4394790ead0c7ef3b9f7d
iyari02medina <68793530+iyari02medina@users.noreply.github.com>
2022-03-31 12:43:07 -0600

    Cambios en es7

---
+++ 05-ECMAScript6/src/es7/index.js
@@ -0,0 +1,16 @@
+// Validar si se encuentra un elemento dentro de un array
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+
+if(numbers.includes(7)) {
+    console.log("si se encuentra en valor 7")
+} else {
+    console.log("No se encuentra el valor 7")
+}
+
+// elevar un numero a un potencia 
+
+let base =4;
+let exponent = 3;
+let result = base**exponent;
+console.log(result)
